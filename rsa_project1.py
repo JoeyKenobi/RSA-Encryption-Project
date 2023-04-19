fc6081d809bbde5a3e88c7b5fb34c58eda79fafe
JoeyKenobi <joeyr1996@gmail.com>
2023-04-18 20:56:46 -0500

    Added Logger, Added comments and string literals for clarity, changed prints to logger.info, added dynamic filepath using os import

--- rsa_project1.py
+++ rsa_project1.py
@@ -1,8 +1,26 @@
 import random
 import math
+import logging
+import os
 
+
+"""setting up logger, and dynamically setting the path variable"""
+# Get the path, directory, and filename of the current Python file
+current_file_path = os.path.abspath(__file__)
+log_directory = os.path.dirname(current_file_path)
+log_filename = os.path.splitext(os.path.basename(current_file_path))[0]
+
+# Set the log file name as 'filename.log' in the same directory as the Python file, and configure the logger
+log_file = os.path.join(log_directory, log_filename + '.log')
+logging.basicConfig(filename=log_file, level=logging.INFO,
+                    format='%(asctime)s - %(levelname)s - %(message)s')
+
+logger = logging.getLogger(__name__)
+
+
+"""Calculating the public and private keys"""
 def is_prime(num):
-    """Check if a number is prime."""
+    """Check if a random large number generated below is prime."""
     if num <= 1:
         return False
     for i in range(2, int(math.sqrt(num)) + 1):
@@ -12,7 +30,7 @@ def is_prime(num):
 
 
 def generate_prime_number():
-    """Generate a random prime number."""
+    """Generate a random large number, check if it is prime using the is_prime function."""
     while True:
         num = random.randint(100, 1000)
         if is_prime(num):
@@ -20,7 +38,7 @@ def generate_prime_number():
 
 
 def gcd(a, b):
-    """Calculate the greatest common divisor of two numbers."""
+    """Calculate the greatest common divisor of two generated prime numbers."""
     if b == 0:
         return a
     else:
@@ -28,7 +46,7 @@ def gcd(a, b):
 
 
 def mod_inverse(a, m):
-    """Calculate the modular inverse of a number."""
+    """Calculate the modular inverse of a prime number."""
     m0 = m
     y = 0
     x = 1
@@ -54,7 +72,7 @@ def mod_inverse(a, m):
 
 
 def generate_rsa_key_pair():
-    """Generate RSA key pair."""
+    """Generate RSA key pair"""
     p = generate_prime_number()
     q = generate_prime_number()
 
@@ -80,17 +98,17 @@ def encrypt(text_fron_user, n, e):
     return encrypted_text.strip()
 
 
-# Main
-print("RSA Encryption Program")
-
+"""log results"""
+# Main program
+logger.info("RSA Encryption Program")
 text_fron_user = input("Enter text to encrypt: ")
 
 # Generate RSA key pair
 n, e, d = generate_rsa_key_pair()
-print("Generated RSA key pair:")
-print("Public key (n, e): {}".format((n, e)))
-print("Private key (n, d): {}".format((n, d)))
+logger.info("Generated RSA key pair:")
+logger.info("Public key (n, e): {}".format((n, e)))
+logger.info("Private key (n, d): {}".format((n, d)))
 
 # Encrypt the input text from the user
 encrypted_text = encrypt(text_fron_user, n, e)
-print("Encrypted text: {}".format(encrypted_text))
+logger.info("Encrypted text: {}".format(encrypted_text))
